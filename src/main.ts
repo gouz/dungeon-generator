213c58d2c13ba6938b105f434b6bd5796196f318
Sylvain Gougouzian <sylvain.gougouzian@gmail.com>
2026-01-08 23:15:39 +0100

    fix: decay & canvas size

--- src/main.ts
+++ src/main.ts
@@ -5,18 +5,24 @@ document.addEventListener("DOMContentLoaded", async () => {
   const ctx = canvas.getContext("2d") as CanvasRenderingContext2D;
 
   const TILE_SIZE = 64;
-  const RATIO = 1;
+  const RATIO = 0.5;
+  const DECAY = 4;
+
+  canvas.width = 20 * (TILE_SIZE - DECAY) + DECAY;
+  canvas.height = 10 * (TILE_SIZE - DECAY) + DECAY;
 
   const drawTile = (source: number[], destGridX: number, destGridY: number) => {
     const [sourceX, sourceY] = source;
     ctx.drawImage(
       tilesheet,
-      sourceX! * TILE_SIZE,
-      sourceY! * TILE_SIZE,
+      sourceX * TILE_SIZE,
+      sourceY * TILE_SIZE,
       TILE_SIZE,
       TILE_SIZE,
-      destGridX * TILE_SIZE * RATIO - (destGridX !== 0 ? destGridX * 4 : 0),
-      destGridY * TILE_SIZE * RATIO - (destGridY !== 0 ? destGridY * 4 : 0),
+      destGridX * TILE_SIZE * RATIO -
+        (destGridX !== 0 ? destGridX * DECAY : 0) * RATIO,
+      destGridY * TILE_SIZE * RATIO -
+        (destGridY !== 0 ? destGridY * DECAY : 0) * RATIO,
       TILE_SIZE * RATIO,
       TILE_SIZE * RATIO,
     );
@@ -30,8 +36,8 @@ document.addEventListener("DOMContentLoaded", async () => {
 
   const grounds: string[] = [...Object.keys(tiles.grounds)];
 
-  for (let x = 0; x < 20; x++)
-    for (let y = 0; y < 10; y++)
+  for (let x = 0; x < 20 / RATIO; x++)
+    for (let y = 0; y < 10 / RATIO; y++)
       drawTile(
         tiles.grounds[grounds[Math.floor(Math.random() * grounds.length)]],
         x,
